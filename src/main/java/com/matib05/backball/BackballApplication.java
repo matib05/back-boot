9bbd8f8bfe56732cdc6ba88c25c0e9bd41e7fe6e
Matib <matib05@gmail.com>
2018-05-10 09:04:25 -0400

    initial commit

---
+++ src/main/java/com/matib05/backball/BackballApplication.java
@@ -0,0 +1,12 @@
+package com.matib05.backball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackballApplication.class, args);
+	}
+}
---
+++ src/test/java/com/matib05/backball/BackballApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matib05.backball;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackballApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
